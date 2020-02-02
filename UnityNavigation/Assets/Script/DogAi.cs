bbef140cde506dafc0e57d67c8daabcf7a3d38bd
Timeteddy <59497727+Timeteddy@users.noreply.github.com>
2020-02-02 16:51:16 +0800

    自動循路完成

---
+++ UnityNavigation/Assets/Script/DogAi.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DogAi : MonoBehaviour
+{
+    public Transform aims;
+    public NavMeshAgent agent;
+    public Animator anim;
+    private bool iswalk;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            iswalk = true;
+        }
+
+        if (iswalk)
+        {
+            Move();
+        }
+
+        if (agent.hasPath)
+        {
+            anim.SetBool("Run", true);
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
+    }
+
+    public void Move()
+    {
+        agent.SetDestination(aims.position);
+
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            aims.GetComponent<aims>().changPosition();
+
+            iswalk = false;
+        }
+    }
+}
---
+++ UnityNavigation/Assets/Script/aims.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aims : MonoBehaviour
+{
+    private float bottom_right = 4.0f;
+    private float bottom_lift = -14.7f;
+
+    private float bottom_up = 4.0f;
+    private float bottom_down = -14.7f;
+
+    void Awake()
+    {
+        changPosition();
+    }
+
+    /// <summary>
+    /// 隨機位置
+    /// </summary>
+    public void changPosition()
+    {
+        float pos_X = Random.Range(bottom_lift, bottom_right);
+        float pos_Z = Random.Range(bottom_down, bottom_up);
+
+        transform.position = new Vector3(pos_X, transform.position.y, pos_Z);
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "wall")
+        {
+            changPosition();
+        }
+    }
+}
